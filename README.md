f9c358ba19f6a0332f36649010a8fca5fc393b94
Lucas Z <luckybase@gmail.com>
2015-04-05 16:51:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Usage
+-----
+
+First, get superuser privileges with 'sudo -s' command. Then, run 'setup.py' typing in a terminal:
+    `python setup.py`
+or
+    `./setup.py`
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from os import system
+
+
+f = open('list.txt', 'r').readlines()
+for line in f:
+    flag = False
+    command = line.split('|')
+    cmd = command[0]
+    app = command[1]
+
+    if cmd == 'ppa':
+        code = 'sudo add-apt-repository '
+    elif cmd == 'update':
+        code = 'sudo apt-get update'
+    elif cmd == 'apt':
+        code = 'sudo apt-get install '
+    elif cmd == 'pip':
+        code = 'pip install '
+
+    #if flag == False:
+    #    system('sudo -s')
+    #    flag = True
+
+    install = code + app
+    system(install)
