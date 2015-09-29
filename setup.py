3d0a19b509100c184d659a3cd311b229826ad96c
Lucas Z <luckybase@gmail.com>
2015-09-28 23:37:27 -0300

    Added parameter for unattended installation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from os import system
 
 
-f = open('list.txt', 'r').readlines()
+f = open('requirements.txt', 'r').readlines()
 for line in f:
     flag = False
     command = line.split('|')
@@ -16,16 +16,18 @@ for line in f:
         code = 'sudo apt-get update '
     elif cmd == 'upgrade':
         code = 'sudo apt-get upgrade'
+    elif cmd == 'pipu':
+        code = 'sudo pip install -U '
     elif cmd == 'apt':
         code = 'sudo apt-get install '
     elif cmd == 'remove':
         code = 'sudo apt-get remove '
     elif cmd == 'pip':
-        code = 'pip install '
+        code = 'sudo pip install '
 
     #if flag == False:
     #    system('sudo -s')
     #    flag = True
 
-    install = code + app
+    install = 'yes | ' + code + app
     system(install)
